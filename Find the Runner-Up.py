2c2f7593c875c5c9eef3c319cc4defdf585a5581
youjisang <jisangman89@gmail.com>
2018-02-16 23:38:36 +0900

    2018/02/16

--- Find the Runner-Up.py
+++ Find the Runner-Up.py
@@ -3,12 +3,10 @@ if __name__ == '__main__':
     arr = map(int, input().split())
     
     set_t= set(arr)
-    print("set_t",set_t)
     list_t = list(set_t)
     list_t.sort()
-    print("list_t",list_t)
-    print("len(list_t)",len(list_t))
+    print(list_t[len(list_t)-2])
  
    
-
+# print sorted(list(set(nums)))[-2] 이렇게 더욱 줄일 수 있음.
     
