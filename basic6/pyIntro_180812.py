edeee6a183f0948385bc6b14bbeac8f6f967da0b
youjisang <jisangman89@gmail.com>
2018-08-12 23:58:37 +0900

    2018/08/12

---
+++ basic6/pyIntro_180812.py
@@ -0,0 +1,41 @@
+# 딕셔너리 컴프리헨션
+word = 'letters'
+# letter_counts = {letter: word.count(letter) for letter in word}
+letter_counts = {letter: word.count(letter) for letter in set(word)}
+print(letter_counts)
+
+# 셋 컴프리헨션
+a_set = {number for number in range(1, 6) if number % 3 == 1}
+print(a_set)
+
+# 제너레이트 컴프리헨션
+number_thing = (number for number in range(1, 6))
+# for number in number_thing:
+#     print(number)
+number_list = list(number_thing)
+print(number_list)
+
+
+# 함수
+def make_a_sound():
+    print('quack')
+
+
+make_a_sound()
+
+
+def agree():
+    return True
+
+
+if agree():
+    print('splendid!')
+else:
+    print('That was unexpected.')
+
+
+def menu(wine, entree, dessert):
+    return {'wine': wine, 'entree': entree, 'dessert': dessert}
+
+
+print(menu("chardonnay", 'chicken', 'cake'))
