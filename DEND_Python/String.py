4370f77af7605bf8c50763db457f19a6a7fb2332
youjisang <jisangman89@gmail.com>
2019-05-01 22:01:49 +0900

    문법 체크 중....

---
+++ DEND_Python/String.py
@@ -0,0 +1,23 @@
+first_word = "Hello"
+second_word = "There"
+
+print(first_word + second_word)
+print((first_word + second_word) * 3)
+print(len(first_word))
+print(first_word[2])
+
+ford_quote = 'Whether you think you can, or you think you can\'t--you\'re right.'
+ford_quote = "Whether you think you can, or you think you can\'t--you\'re right."
+
+print(ford_quote)
+
+coconut_count = "34"
+mango_count = "15"
+tropical_fruit_count = coconut_count + mango_count
+print(tropical_fruit_count)
+# print(len(835)) error
+
+
+print(type(633))
+print(type("633"))
+print(type(633.0))
---
+++ PFDA_1/np.py
@@ -0,0 +1,5 @@
+import numpy as np
+import datetime
+
+data = {i : np.random.randn() for i in range(7)}
+print(data)
