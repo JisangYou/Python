e8d62481f369150315d1b2651ab62b4f21284ae7
youjisang <jisangman89@gmail.com>
2019-05-08 00:16:20 +0900

    datastructure join, append

--- DEND_Python/datastructure.py
+++ DEND_Python/datastructure.py
@@ -2,14 +2,14 @@ list_of_random_things = [1, 3.4, 'a string', True]
 print(list_of_random_things[-2])
 print(list_of_random_things[:3])
 print(list_of_random_things[2:])
-print(3.4 in list_of_random_things) # wow
+print(3.4 in list_of_random_things)  # wow
 
 month = 8
-days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 # use list indexing to determine the number of days in month
 
-num_days = days_in_month[month-1]
+num_days = days_in_month[month - 1]
 
 print(num_days)
 
@@ -21,3 +21,16 @@ eclipse_dates = ['June 21, 2001', 'December 4, 2002', 'November 23, 2003',
 # TODO: Modify this line so it prints the last three elements of the list
 print(eclipse_dates[7:])
 print(eclipse_dates[-3:])
+
+sizes = [15, 6, 89, 34, 65, 35]
+print(sorted(sizes, reverse=True))
+
+nautical_directions = "\n".join(["fore", "aft", "starboard", "port"])
+print(nautical_directions)
+
+name = "-".join(["García", "O'Kelly"])
+print(name)
+
+letters = ['a', 'b', 'c', 'd']
+letters.append('z')
+print(letters)
