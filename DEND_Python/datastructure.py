99f202af489df01a5c6b55d9c9a54af1ea0c8c31
youjisang <jisangman89@gmail.com>
2019-05-09 23:59:43 +0900

    datastructure List

--- DEND_Python/datastructure.py
+++ DEND_Python/datastructure.py
@@ -34,3 +34,14 @@ print(name)
 letters = ['a', 'b', 'c', 'd']
 letters.append('z')
 print(letters)
+
+
+a = [1, 5, 8]
+b = [2, 6, 9, 10]
+c = [100, 200]
+
+print(max([len(a), len(b), len(c)]))
+print(min([len(a), len(b), len(c)]))
+
+names = ["Carol", "Albert", "Ben", "Donna"]
+print(" & ".join(sorted(names)))
