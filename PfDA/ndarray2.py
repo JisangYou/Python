da81afde22b89e001f5221d4caaa3938fbe469da
youjisang <jisangman89@gmail.com>
2019-07-04 00:18:37 +0900

    Numpy, ndarray swapaxes

--- PfDA/ndarray2.py
+++ PfDA/ndarray2.py
@@ -5,3 +5,18 @@ import numpy as np
 d = np.arange(20).reshape(4, 5)
 print(d[0:3, 1:3])
 
+# 팬시색인
+
+arr = np.empty((8, 4))
+for i in range(8):
+    arr[i] = i
+print(arr[[4, 3, 0, 6]])
+
+arr2 = np.arange(15).reshape((3, 5))
+print("arr2\n", arr2)
+
+arr3 = np.arange(16).reshape((2, 2, 4))
+
+print(arr3.transpose((1, 0, 2)))
+# 축 바꾸기
+print(arr3.swapaxes(1, 2))
