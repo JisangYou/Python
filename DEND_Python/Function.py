ae427fe6f73713ca90b922b8347c9ea6c717778f
youjisang <jisangman89@gmail.com>
2019-09-07 23:54:39 +0900

    list, zip, function 학습

--- DEND_Python/Function.py
+++ DEND_Python/Function.py
@@ -1,3 +1,32 @@
 def cylinder_volume(height, radius=5):
     pi = 3.14159
     return height * pi * radius ** 2
+
+
+def readable_timedelta(days):
+    # use integer division to get the number of weeks
+    weeks = days // 7
+    # use % to get the number of days that remain
+    remainder = days % 7
+    return "{} week(s) and {} day(s).".format(weeks, remainder)
+
+
+# test your function
+print(readable_timedelta(10))
+
+
+numbers = [
+              [34, 63, 88, 71, 29],
+              [90, 78, 51, 27, 45],
+              [63, 37, 85, 46, 22],
+              [51, 22, 34, 11, 18]
+           ]
+
+averages = list(map(lambda x: sum(x) / len(x), numbers))
+print(averages)
+
+
+cities = ["New York City", "Los Angeles", "Chicago", "Mountain View", "Denver", "Boston"]
+
+short_cities = list(filter(lambda x: len(x) < 10, cities))
+print(short_cities)
