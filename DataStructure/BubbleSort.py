9582305b858cd637b3e83b74b58c90531adfd089
youjisang <jisangman89@gmail.com>
2019-10-19 20:17:22 +0900

    BubbleSort, QuickSort

---
+++ DataStructure/BubbleSort.py
@@ -0,0 +1,12 @@
+def bubbleSort(alist):
+    for passnum in range(len(alist) - 1, 0, -1):
+        for i in range(passnum):
+            if alist[i] > alist[i + 1]:
+                temp = alist[i]
+                alist[i] = alist[i + 1]
+                alist[i + 1] = temp
+
+
+x = [5, 2, 8, 6, 1, 9, 3, 7]
+bubbleSort(x)
+print(x)
